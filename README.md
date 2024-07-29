766805837dc2d499177808ab1c3cb1740dd6327f
T.Nuth <thiagomoreira@Mac-Pro-de-Thiago.local>
2024-07-28 21:56:02 -0300

    Iniciando o Banco de Dados

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Chat-App---JavaScript
+
 Desenvolvendo uma aplicação de chat em tempo real ou uma plataforma de colaboração ao vivo que permite comunicação instantânea entre os usuários.
+
+## Como Executar o Projeto
+
+### Pré-requisitos
+
+- Node.js instalado
+- npm instalado
+
+### Passos para Rodar o Projeto
+
+1. **Clone o Repositório**:
+   ```bash
+   git clone https://github.com/seu-usuario/chat-app.git
+   cd chat-app
+   ```
--- server.js
+++ server.js
@@ -2,10 +2,16 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const path = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
+
+const mongoURI = 'mongodb://localhost:27017/chat-app';
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB conectado'))
+    .catch(err => console.log(err));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
