ca524460c16e972e05e5783c7377ae8ed117a920
T.Nuth <thiagomoreira@Mac-Pro-de-Thiago.local>
2024-07-28 21:31:35 -0300

    Primeira parte finalizada

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+  console.log('usuário conectou');
+  socket.on('disconnect', () => {
+    console.log('usuário desconectou');
+  });
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+}); 
