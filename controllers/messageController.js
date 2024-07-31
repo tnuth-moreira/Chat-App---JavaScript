142ae351f3b221714a77f89d58a742609dd4637b
T.Nuth <thiagomoreira@Mac-Pro-de-Thiago.local>
2024-07-30 22:09:28 -0300

    Melhorando a criacao de senha ao criar usuario

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,21 +1,29 @@
 const Message = require('../models/Message');
+const User = require('../models/User');
 
 exports.sendMessage = async (req, res) => {
   const { userId, content } = req.body;
 
   try {
-    const message = new Message ({ user: userId, content });
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: 'Usuário não encontrado' });
+    }
+
+    const message = new Message({ user: userId, content });
     await message.save();
-    res.status(201).json({ message: 'Mensagem enviada com sucesso' });
+
+    res.status(201).json(message);
   } catch (error) {
-    res.status(500).json({ message: 'Erro no servidor' });
+    res.status(500).json({ message: 'Erro ao enviar mensagem' });
   }
 };
 
 exports.getMessages = async (req, res) => {
   try {
-    const messages = await Message.find().populate('user', 'username').sort({ createdAt: -1 });
+    const messages = await Message.find().populate('user', 'username');
+    res.status(200).json(messages);
   } catch (error) {
-    res.status(500).json({ message: 'Erro no servidor' });
+    res.status(500).json({ message: 'Erro ao buscar mensagens' });
   }
 };
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,25 +1,35 @@
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
+const { body, validationResult } = require('express-validator');
 
-exports.register = async (req, res) => {
-  const { username, password } = req.body;
-  
-  try {
-    let user = await User.findOne ({ username });
-    if (user) {
-      return res.status(400).json({ message: 'Usuário já existe' });
+exports.register = [
+  body('username').notEmpty().withMessage('Username é obrigatório'),
+  body('password').isLength({ min: 6 }).withMessage('Senha deve ter pelo menos 6 caracteres'),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-    user = new User ({ username, password });
 
-    const salt = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(password, salt);
+    const { username, password } = req.body;
 
-    await user.save();
-    res.status(201).json({ message: 'Usuário criado com sucesso' });
-  } catch (error) {
-    res.status(500).json({ message: 'Erro no servidor' });
+    try {
+      let user = await User.findOne({ username });
+      if (user) {
+        return res.status(400).json({ message: 'Usuário já existe' });
+      }
+      user = new User({ username, password });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+      res.status(201).json({ message: 'Usuário criado com sucesso' });
+    } catch (error) {
+      res.status(500).json({ message: 'Erro no servidor' });
+    }
   }
-};
+];
 
 exports.login = async (req, res) => {
   const { username, password } = req.body;
@@ -37,6 +47,6 @@ exports.login = async (req, res) => {
 
     res.status(200).json({ message: 'Login bem-sucedido' });
   } catch (error) {
-      res.status(500).json({ message: 'Erro no servidor' });
+    res.status(500).json({ message: 'Erro no servidor' });
   }
 };
