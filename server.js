1b3beb9342634bf47ddc082c6c08cc3c9b659cf6
T.Nuth <thiagomoreira@Mac-Pro-de-Thiago.local>
2024-07-30 22:34:24 -0300

    Finalizando o backend para começar o frontend

--- server.js
+++ server.js
@@ -8,16 +8,15 @@ const passport = require('passport');
 
 const userRoutes = require('./routes/userRoutes');
 const messageRoutes = require('./routes/messageRoutes');
-const { error } = require('console');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
 const mongoURI = 'mongodb://localhost:27017/chat-app';
-mongoose.connect(mongoURI)
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB conectado'))
-    .catch(error => console.log('Erro na conexão com MongoDB', error));
+    .catch(err => console.log(err));
 
 require('./config/passportConfig')(passport);
 
@@ -30,16 +29,16 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(express.json());    
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/users', userRoutes);
 app.use('/api/messages', messageRoutes);
 
 io.on('connection', (socket) => {
-  console.log('usuário conectou');
+  console.log('Usuário conectou');
   socket.on('disconnect', () => {
-    console.log('usuário desconectou');
+    console.log('Usuário desconectou');
   });
   socket.on('chat message', (msg) => {
     io.emit('chat message', msg);
@@ -49,4 +48,4 @@ io.on('connection', (socket) => {
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
-}); 
+});
